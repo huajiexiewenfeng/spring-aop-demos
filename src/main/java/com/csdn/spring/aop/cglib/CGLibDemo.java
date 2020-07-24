ce8edffc0e40cda6b996215eb55c7dc22fcef0b2
Administrator <420842659@qq.com>
2020-07-24 09:28:43 +0800

    CGLib 示例

---
+++ src/main/java/com/csdn/spring/aop/cglib/CGLibDemo.java
@@ -0,0 +1,60 @@
+package com.csdn.spring.aop.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * CGLib 示例
+ *
+ * @author ：xwf
+ * @date ：Created in 2020-7-24 9:10
+ */
+public class CGLibDemo {
+
+    static class Car {
+        public void run() {
+            System.out.println("the car is running");
+        }
+    }
+
+    static class CglibProxy<T> implements MethodInterceptor {
+
+        private T target;// 代理对象
+
+        public T getInstance(T target) {
+            this.target = target;
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(target.getClass());
+            enhancer.setCallback(this);
+            return (T) (enhancer.create());
+        }
+
+        @Override
+        public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+            preOperation();
+            Object retVal = methodProxy.invokeSuper(o, objects);
+            postOperation();
+            return retVal;
+        }
+
+        private void postOperation() {
+            System.out.println("后置处理...");
+        }
+
+        private void preOperation() {
+            System.out.println("前置处理...");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CglibProxy<Car> cglibProxy = new CglibProxy<>();
+        Car car = cglibProxy.getInstance(new Car());
+        car.run();
+    }
+
+}
+
